193462232532b3c6a4abe28e5452576aa9da8093
GrishaB12 <154069149+GrishaB12@users.noreply.github.com>
2026-05-08 18:30:14 +0300

    Create yut.py

---
+++ yut.py
@@ -0,0 +1,7 @@
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
